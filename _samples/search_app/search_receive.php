4f4d43c6dea80ad3844a271f50d8474f4c0f3df9
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-03-30 11:14:44 +0900

    P112_search_app

--- _samples/search_app/search_receive.php
+++ _samples/search_app/search_receive.php
@@ -1,21 +1,38 @@
 <?php 
+
+// POSTの受信はコードの最初に行う（くらに重要なこと）。
+// サーバーから送信されてデータが『POST』という文字列と同じなら。。。
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'];
+}
+
 $dsn = 'mysql:dbname=member;host=localhost;charset=utf8';
 $user = 'root';
 $password = '';
 
+// html内で持ち運ぶための変数を初期化する。
+// $data = [];
+// 配列で初期化。
+$data = array();
+
 try {
   $dbh = new PDO($dsn, $user, $password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "SELECT * FROM user";
+  // SQL文を定義する。
+  $sql = "SELECT id, name FROM user WHERE name LIKE :name";
 
   // プリペアドステートメントを作成
   $stmt = $dbh->prepare($sql);
 
+  // db_insert_with_variable.phpでやったのはパラメータと変数を結びつけるやつ
+  // $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+  // $stmt->bindParam(':age', $age, PDO::PARAM_INT);
+
+  $stmt->bindValue(':name', '%'.$name.'%', PDO::PARAM_STR);
+
   // SQL文を実行する（インサートする）。
   $stmt->execute();
 
-  // 配列で初期化。
-  $data = array();
   // rowCount関数で行数を取得する。
   $count = $stmt->rowCount();
   // fetch関数の引数にPDO::FETCH_ASSOCを与えて一行ずつデータを取得する。
@@ -28,9 +45,7 @@ try {
   //   $data[] = $row;
   // }
   while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    var_dump('<hr>');
-    var_dump($row);
-    // $data[] = $row;
+    $data[] = $row;
   }
   
   // echo 'The process has been completed!';
@@ -46,31 +61,29 @@ try {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Search App –RECEIVE–</title>
   <link rel="stylesheet" href="https://unpkg.com/destyle.css@4.0.0/destyle.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link rel="stylesheet" href="./style.css">
-  <script src="./behavior.js" defer></script>
 </head>
 
 <body>
   <div class="container">
-    <p>メンバーは<?php echo $count ?>名</p>
+    <h1>会員データ一覧</h1>
+    <p><?php echo $count ?>件見つかりました。</p>
     <ul>
-      <li>
-        <div class="item header">ID</div>
-        <div class="item header">氏名</div>
-        <div class="item header">年齢</div>
-        <div class="item header">メールアドレス</div>
+      <li class="header">
+        <div class="item">ID</div>
+        <div class="item">氏名</div>
       </li>
+
         <?php foreach ($data as $item): ?>
       <li>
         <div class="item"><?php echo $item['id'] ?></div>
         <div class="item"><?php echo $item['name'] ?></div>
-        <div class="item"><?php echo $item['age'] ?></div>
-        <div class="item"><?php echo $item['email'] ?></div>
       </li>
         <?php endforeach; ?>
+
     </ul>
   </div>
 </body>
